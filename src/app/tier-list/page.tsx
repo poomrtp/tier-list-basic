59c60c7afc622744d8a55b1f0bcc284fda33c426
Ratthapon Sananaur <poom8165@gmail.com>
2023-11-12 18:51:06 +0700

    [fixed]: width container

--- src/app/tier-list/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import TierListContainer from '@/components/tier-list/tier-list-container';
-import React from 'react';
-
-function TierListPage() {
-  return <TierListContainer />;
-}
-
-export default TierListPage;
--- src/components/tier-list/tier-list-container.tsx
+++ src/components/tier-list/tier-list-container.tsx
@@ -5,7 +5,7 @@ import ItemList from './item-list';
 
 function TierListContainer() {
   return (
-    <div className="grid grid-cols-12 gap-4">
+    <div className="grid grid-cols-12 gap-4 w-full">
       <ItemList className="col-span-3" />
       <TierTable className="col-span-9" />
     </div>
